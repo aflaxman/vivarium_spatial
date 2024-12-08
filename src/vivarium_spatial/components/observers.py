474e224c925e8d7807d993c0bea77c46ad6f6e16
Abraham Flaxman <abie@uw.edu>
2024-12-07 22:57:34 -0800

    ENH: stratified collision count observer

--- src/vivarium_spatial/components/observers.py
+++ src/vivarium_spatial/components/observers.py
@@ -1,15 +1,13 @@
 from typing import List
 import pandas as pd
+from pandas.api.types import CategoricalDtype
 from vivarium.framework.engine import Builder
 from vivarium_public_health.results.observer import PublicHealthObserver
+from vivarium_public_health.results.columns import COLUMNS
 
 
 class CollisionObserver(PublicHealthObserver):
-    """Observes particle collisions in the simulation.
-    
-    This observer computes collision counts for each timestep by tracking
-    particles that collided during the current timestep only.
-    """
+    """Observes particle collisions in the simulation."""
     
     ##############
     # Properties #
@@ -67,10 +65,9 @@ class CollisionObserver(PublicHealthObserver):
         )
 
     def map_whimsy_categories(self, pop: pd.DataFrame) -> pd.Series:
-        """Maps whimsy values to categories"""
-        # Create bins based on whimsy values
-        cuts = pd.qcut(pop.whimsy, q=3, labels=['low', 'medium', 'high'])
-        return cuts
+        """Maps whimsy values to categories using fixed cut points"""
+        bins = [0, 1/3, 2/3, 1]
+        return pd.cut(pop.whimsy, bins=bins, labels=['low', 'medium', 'high'], include_lowest=True)
 
     ###############
     # Aggregators #
@@ -81,3 +78,32 @@ class CollisionObserver(PublicHealthObserver):
         current_time = self.clock()
         collided_this_step = pop.last_collision_time == current_time
         return len(pop[collided_this_step])
+
+    ##############################
+    # Results formatting methods #
+    ##############################
+
+    def format(self, measure: str, results: pd.DataFrame) -> pd.DataFrame:
+        """Format the results dataframe"""
+        results = results.reset_index()
+        if 'whimsy' in results.columns:
+            results.rename(columns={'whimsy': COLUMNS.SUB_ENTITY}, inplace=True)
+        return results
+
+    def get_measure_column(self, measure: str, results: pd.DataFrame) -> pd.Series:
+        """Get the measure column values"""
+        return pd.Series('collision_count', index=results.index)
+
+    def get_entity_type_column(self, measure: str, results: pd.DataFrame) -> pd.Series:
+        """Get the entity_type column values"""
+        return pd.Series('particle', index=results.index)
+
+    def get_entity_column(self, measure: str, results: pd.DataFrame) -> pd.Series:
+        """Get the entity column values"""
+        return pd.Series('collision', index=results.index)
+
+    def get_sub_entity_column(self, measure: str, results: pd.DataFrame) -> pd.Series:
+        """Get the sub_entity column values"""
+        if COLUMNS.SUB_ENTITY in results.columns:
+            return results[COLUMNS.SUB_ENTITY].astype(CategoricalDtype())
+        return pd.Series('all', index=results.index, dtype=CategoricalDtype())
